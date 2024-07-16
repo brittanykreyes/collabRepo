b074249ae6192be48ba7faef9b2dd599e2137b1b
Hammarou <139810496+Hammarou@users.noreply.github.com>
2024-07-15 21:15:08 -0400

    changes to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    fontFamily: {
+     'sans': ['ui-sans-serif', 'system-ui', 'Helvetica', 'Arial', 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji" ],
+     'serif': ['Arial','ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+     'Monaco':['Monaco'],
+     'mono' : ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
+     'display': ['Oswald'],
+     'body': ['Open Sans'],
+    }
+  },
+
   content: [
     './pages/index.html',
     './src/**/*.{html}'
